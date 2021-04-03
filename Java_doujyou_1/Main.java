eb8d9f6ae30e6e34b19fcf2a62664131d3af52cf
choco-monaka <amx004qubeley105@gmail.com>
2021-04-03 02:22:35 +0000

    java

---
+++ Java_doujyou_1/Main.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.print("何人分の情報を入力しますか：");
+    int countPeople = scanner.nextInt();
+    int maxAge = 0;
+    int totalAge = 0;
+    for (int i = 0; i < countPeople; i++) {
+      System.out.println(i + 1 + "人目");
+      System.out.print("名前：");
+      String firstName = scanner.next();
+
+      System.out.print("名字：");
+      String lastName = scanner.next();
+
+      System.out.print("年齢：");
+      int age = scanner.nextInt();
+
+      System.out.print("身長(m)：");
+      double height = scanner.nextDouble();
+
+      System.out.print("体重(kg)：");
+      double weight = scanner.nextDouble();
+
+      // ageがmaxAgeを超えた場合、maxAgeにageを代入する
+      if (age > maxAge) {
+        maxAge = age;
+      }
+
+      // for文の中で、合計年齢に入力された年齢を足していく
+      totalAge += age;
+      Person.printData(firstName, lastName, age, height, weight);
+
+    }
+    System.out.println("最高年齢は" + maxAge + "歳です");
+    System.out.println("平均年齢は" + totalAge / countPeople + "歳です");
+
+  }
+
+}
+
+
