28145bdaf1fdb1d5cd9aa0fa199e9cc7e2e09050
YUKIHISAMATSUOKA <amx004qubeley105@gmail.com>
2021-04-07 21:09:40 +0900

    push_test

---
+++ practice/Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello Java");
+
+    int x;
+    x = 100;
+    x++;
+    System.out.println(x);
+    String s = "あいうえおかきくけこ";
+    System.out.println(s);
+
+  }
+}
