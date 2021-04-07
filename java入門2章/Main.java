2fe965f955641762028661d94e58ffa86a954574
YUKIHISAMATSUOKA <amx004qubeley105@gmail.com>
2021-04-07 21:30:50 +0900

    ソース管理からコミット

--- java入門2章/Main.java
+++ java入門2章/Main.java
@@ -4,6 +4,10 @@ public class Main {
     int b = 3;
     int m = Math.max(a, b);
     System.out.println("比較実験" + a + "と" + b + "とで大きいほうは" + m);
+    
+    // 乱数を発生させる式
+    int r = new java.util.Random().nextInt(10);
+    System.out.println(r);
   }
 
 }
