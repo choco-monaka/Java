4df41a761be7bc1bfc54e8a8322f22b2830a48e5
YUKIHISAMATSUOKA <amx004qubeley105@gmail.com>
2021-04-11 22:05:52 +0900

    12章finish

--- java入門12章/src/Character.java
+++ java入門12章/src/Character.java
@@ -1,14 +1,8 @@
-// 抽象クラスになったので、このクラスはnew利用できない。
-public abstract class Character {
-
+public abstract class Character implements Life {
   String name;
   int hp;
-
   public void run() {
-    System.out.println(this.name + "は、逃げ出した!");
+    System.out.println(this.name + "は逃げ出した");
   }
-
-  // Characterが必ず持つべき、メソッドだが処理内容が未確定なのでabstractを使用する。
   public abstract void attack(Matango m);
-
 }
