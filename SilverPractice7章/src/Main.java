6dd5f4355ec9fa41639cb03d03da04897f88767d
YUKIHISAMATSUOKA <amx004qubeley105@gmail.com>
2021-04-23 18:41:18 +0900

    start

--- SilverPractice7章/src/Main.java
+++ SilverPractice7章/src/Main.java
@@ -1,6 +1,18 @@
+
 public class Main {
   public static void main(String[] args) {
-    // 7章
+
+    // ここから問題
+    // 1234567890 の正の約数のうち、500000以下のものを全て足し合わせたときの和を求める
+    int x = 1234567890;
+    int total = 0;
+    for (int i = 0; i < 500000; i++) {
+      if (x % i == 0) {
+        total = total + i;
+      }
+    }
+
+    System.out.println(total);
 
   }
 }
