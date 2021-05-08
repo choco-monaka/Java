db72030069de07dfa53ab739ed8df716da72980d
YUKIHISAMATSUOKA <amx004qubeley105@gmail.com>
2021-05-09 08:48:50 +0900

    practice

--- math_practice7/src/Main.java
+++ math_practice7/src/Main.java
@@ -1,5 +1,5 @@
 import java.util.Arrays;
-import java.util.Collections;
+import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
@@ -18,11 +18,36 @@ public class Main {
       System.out.print(value + " ");
     }
 
-    int x = 0;
+    int y = 0;
     for (int i = 1; i <= 64; i++) {
-      x += i * i * i * i;
+      y += i * i * i * i;
     }
-    System.out.println(x);
+    System.out.println(y);
+
+    // バイナリーサーチ
+    Scanner scanner = new Scanner(System.in);
+    int a[] = { 2, 10, 11, 12, 39, 43, 45, 52, 57, 63, 65, 66, 73, 76, 83, 97 };
+    System.out.println("データを入力して下さい: ");
+    int x = scanner.nextInt();
+    int pos = -1;
+    int lower = 0;
+    int upper = a.length - 1;
+    while (lower <= upper) {
+      int mid = (lower + upper) / 2;
+      if (a[mid] == x) {
+        pos = mid;
+        break;
+      } else if (a[mid] < x) {
+        lower = mid + 1;
+      } else {
+        upper = mid - 1;
+      }
+    }
+    if (pos < 0) {
+      System.out.println("見つかりません");
+    } else {
+      System.out.println(x + "は" + pos + "番目です");
+    }
 
   }
 }
