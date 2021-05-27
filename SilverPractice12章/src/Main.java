cda60fc86ea5e397a69f21726dadf449d7f675b3
YUKIHISAMATSUOKA <amx004qubeley105@gmail.com>
2021-05-28 07:44:12 +0900

    Silver 12

--- SilverPractice12章/src/Main.java
+++ SilverPractice12章/src/Main.java
@@ -1,20 +1,6 @@
 public class Main {
   public static void main(String[] args) {
     // 12章
-    // 素数計算
-    int x = 0;
-
-    for (int i = 0; i <= 10; i++) {
-      System.out.print(i + " ");
-
-      for (int j = 0; j < i / 2; j++) {
-        // if (i % i == 0) && {
-        // x += i;
-        // }
-        System.out.print(j);
-        System.out.println(" ");
-      }
-    }
 
   }
 }
