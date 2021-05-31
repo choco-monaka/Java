bf62ab374bfd2c59f49e780be6d3ad8208d8f490
YUKIHISAMATSUOKA <amx004qubeley105@gmail.com>
2021-05-31 20:24:45 +0900

    練習

--- blackjeck/src/Blackjack.java
+++ blackjeck/src/Blackjack.java
@@ -1,8 +1,9 @@
 package net.sejuku;
 
+import java.util.Collections;
 import java.utill.ArrayList;
 import java.utill.List;
-import java.utillCcollections;
+import java.utill.Ccollections;
 import java.utill.Scanner;
 
 /*カード枚数は52枚。ジョーカーは含めない。カードの重複が無いように山札を構築する。
@@ -30,16 +31,25 @@ public class Blackjack {
 
     System.out.println("ゲーム開始");
     // 空の山札作成
+    List<Integer> deck = new ArrayList<>(52);
 
     // 山札シャッフル
 
-    // プレイヤー・ディーラーの手札りすとを作成
+    // プレイヤー・ディーラーの手札リストを作成
+    List<Integer> player = new ArrayList<>();
+    List<Integer> dealer = new ArrayList<>();
 
     // プレイヤー・ディーラーがカードを2枚引く
+    player.add(deck.get(0));
+    dealer.add(deck.get(1));
+    player.add(deck.get(2));
+    dealer.add(deck.get(3));
 
     // 山札の進行状況を記録する変数deckCountを定義
+    int deckCount = 4;
 
     // プレイヤーの手札枚数を記録する変数playerHandsを定義
+    int playerHands = 2;
 
     // プレイヤー・ディーラーの手札のポイントを表示
     System.out.println("あなたの1枚目のカードは" + toDescription(player.get(0)));
@@ -58,6 +68,58 @@ public class Blackjack {
     // ポイントを比較する
     System.out.println("あなたのポイントは" + playerPoint);
     System.out.println("ディーラーのポイントは" + dealerPoint);
+
+    System.out.println("引き分け");
+    System.out.println("勝ち");
+    System.out.println("負け");
+
+  }
+
+  // 山札(deck)に値を入れ、シャッフルするメソッド
+  private static void shuffleDeck(List<Integer> deck) {
+
+    // リストに1-52の連番を代入
+    for (int i = 1; i <= 52; i++) {
+      deck.add(i);
+    }
+
+    // 山札をシャッフル
+    Collections.shuffle(deck);
+
+    // リストの中身を確認(デバック用)
+    /*
+     * for (int i = 0; i < deck.size(); i++) { System.out.println(deck.get(i)); }
+     */
+
+  }
+
+  // 手札がバーストしているか判定するメソッド
+  private static int sumPoint(List<Integer> list) {
+
+  }
+
+  // 山札の通し番号を得点計算用のポイントに変換するメソッド J/Q/Kは10とする
+  private static int toPoint(int num) {
+
+  }
+
+  // 山札の数を(スート)の(ランク)の文字列に置き換えるメソッド
+  private static String toDescription(int cardNumber) {
+
+  }
+
+  // 山札の数をカードの数に置き換えるメソッド
+  private static int toNumber(int cardNumber) {
+
+  }
+
+  // カード番号をランク(AやJ.Q.K)に変換するメソッド
+  private static String toRank(int number) {
+
+  }
+
+  // 山札の数をスート(ハートやスペードなどのマーク)に置き換えるメソッド
+  private static String toSuit(int cardNumber) {
 
   }
 
