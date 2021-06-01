f0b7187a8e7aefc5354a805ad0523f8b1a790b7d
YUKIHISAMATSUOKA <amx004qubeley105@gmail.com>
2021-06-01 17:40:50 +0900

    練習

--- blackjeck/src/Blackjack.java
+++ blackjeck/src/Blackjack.java
@@ -86,7 +86,7 @@ public class Blackjack {
     // 山札をシャッフル
     Collections.shuffle(deck);
 
-    // リストの中身を確認(デバック用)
+    // リストの中身を確認(デバック用のみ)
     /*
      * for (int i = 0; i < deck.size(); i++) { System.out.println(deck.get(i)); }
      */
