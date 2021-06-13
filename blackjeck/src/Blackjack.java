ded0f912f6ad80538894d002ca3d03981f4d328f
YUKIHISAMATSUOKA <amx004qubeley105@gmail.com>
2021-06-13 15:04:28 +0900

    練習

--- blackjeck/src/Blackjack.java
+++ blackjeck/src/Blackjack.java
@@ -106,16 +106,40 @@ public class Blackjack {
   // 山札の数を(スート)の(ランク)の文字列に置き換えるメソッド
   private static String toDescription(int cardNumber) {
 
+    String rank = toRank(toNumber(cardNumber));
+    String suit = toSuit(cardNumber);
+
+    return suit + "の" + rank;
+
   }
 
   // 山札の数をカードの数に置き換えるメソッド
   private static int toNumber(int cardNumber) {
+    int number = cardNumber % 13;
+    if (number == 0) {
+      number = 13;
+    }
+
+    return number;
 
   }
 
   // カード番号をランク(AやJ.Q.K)に変換するメソッド.
 
   private static String toRank(int number) {
+    switch (number) {
+      case 1:
+        return "A";
+      case 11:
+        return "J";
+      case 12:
+        return "Q";
+      case 13:
+        return "K";
+      default:
+        String str = String.valueOf(number);
+        return str;
+    }
 
   }
 
